7766c6768f7195438b82df80731516b9b270b8c3
supernova0730 <kuanysheveldar123@gmail.com>
2022-07-27 00:06:31 +0600

    small fix

--- internal/app/app.go
+++ internal/app/app.go
@@ -34,7 +34,7 @@ func Run(conf config.Config) error {
 		TLSEnable: conf.HTTP.TLSEnable,
 		CertFile:  conf.HTTP.CertFile,
 		KeyFile:   conf.HTTP.KeyFile,
-		BotToken:  conf.Bot.BotToken,
+		BotToken:  conf.Bot.Token,
 	}
 
 	return transport.Start(transportConfig, controller)
